9a9282a53b82d2b130ea4ddf385070a8843ce8fe
praveen <spraveenitpro@gmail.com>
2019-06-04 22:32:24 -0700

    Completed Web Cam manipulation with vanilla JS

--- Webcam Fun/scripts.js
+++ Webcam Fun/scripts.js
@@ -24,9 +24,53 @@ function paintToCanvas() {
   canvas.height = height;
   console.log(width, height);
 
-  setInterval(() => {
+  return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    //pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
+    ctx.globalAlpha = 0.1;
+    ctx.putImageData(pixels, 0, 0);
+    //console.log(pixels);
+    //debugger;
   }, 16);
 }
 
+function takePhoto() {
+  //play the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL("image/jpeg");
+  //console.log(data);
+  const link = document.createElement("a");
+
+  link.href = data;
+  link.setAttribute("download", "handsome");
+  link.innerHTML = `<img src="${data}" alt="handsome man" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; //red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; //red
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //red
+    //pixels.data[i + 3] = pixels.data[i + 3] * 0.2; //red
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0] + 100; //red
+    pixels.data[i + 100] = pixels.data[i + 1] - 50; //red
+    pixels.data[i - 150] = pixels.data[i + 2] * 0.5; //red
+    //pixels.data[i + 3] = pixels.data[i + 3] * 0.2; //red
+  }
+  return pixels;
+}
+
 getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
